69ff787cdac9a74fb7554c6196cb4d5f76dffc5d
grupodog <grupodog@gmail.com>
2021-02-11 17:38:12 -0600

    initial_how_it_works

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
 
 function AboutPage() {
   return (
@@ -13,26 +12,28 @@ function AboutPage() {
       />
 
       <section className="flex flex-col items-center md:flex-row">
-        <div className="md:w-2/3 md:mr-8">
-          <blockquote className="pl-4 font-serif leading-loose text-justify border-l-4 border-gray-900">
-            The point is... to live one&apos;s life in the full complexity of
-            what one is, which is something much darker, more contradictory,
-            more of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to the civilized being who glides on
-            the surface and fits smoothly into the world.
-          </blockquote>
-
-          <cite className="block mt-4 text-xs font-bold text-right uppercase">
-            – Thomas Nagel
-          </cite>
-        </div>
-
-        <figure className="w-2/3 md:w-1/3">
-          <img alt="A dog relaxing" src={dogIllustration} />
-        </figure>
+            <div className="mx-auto container">
+                <div className="px-6 lg:px-12 py-6 lg:py-12 relative rounded-3xl overflow-hidden">
+ 
+                    <div className="relative z-10">
+                        <h1 className="text-2xl lg:text-4xl text-black font-bold f-f-l pb-12">Not what you were looking for?</h1>
+                        
+                        <h2 className="text-xl font-semibold f-f-r  text-black pb-3 ">Start an order</h2>
+                        <p className="text-sm pb-7"> Visit our Start your order page and fill out the form. When submitted, you’ll receive a reply with a unique reference number and detailed shipping instructions. If you would like a quote before you ship you can send us the details of your project and we’ll email you back a quote.</p>
+                        <h2 className="text-xl font-semibold f-f-r  text-black pb-3 ">Bringing your team upto speed with real-time collaboration</h2>
+                        <p className="text-sm pb-7"> Hola lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom </p>
+                        <h2 className="text-xl font-semibold f-f-r  text-black pb-3 ">Bringing your team upto speed with real-time collaboration</h2>
+                        <p className="text-sm pb-7"> Hola lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom  impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom </p>
+                        <h2 className="text-xl font-semibold f-f-r  text-black pb-3 ">How to setup your workspace in less than 10 minutes</h2>
+                        <p className="text-sm pb-7"> Hola lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom lorem impsum dolom </p>
+                    </div>
+                </div>
+            </div>
       </section>
     </Layout>
   );
 }
 
 export default AboutPage;
+
+
