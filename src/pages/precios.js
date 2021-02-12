036ce774367b77cc9e8fcbb2a3591035a3eeadb9
grupodog <grupodog@gmail.com>
2021-02-11 18:46:42 -0600

    adds prices card

--- src/pages/precios.js
+++ src/pages/precios.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import dogIllustration from "../images/dog-illustration.svg";
 
 function AboutPage() {
   return (
@@ -13,29 +12,53 @@ function AboutPage() {
       />
 
       <section className="flex flex-col items-center md:flex-row">
-        <div className="md:w-2/3 md:mr-8">
-          <blockquote className="pl-4 font-serif leading-loose text-justify border-l-4 border-gray-900">
-            The point is... to live one&apos;s life in the full complexity of
-            what one is, which is something much darker, more contradictory,
-            more of a maelstrom of more of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to thmore of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to thmore of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to thmore of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to thmore of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to thmore of a maelstrom of impulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to thimpulses and passions, of cruelty, ecstacy,
-            and madness, than is apparent to the civilized being who glides on
-            the surface and fits smoothly into the world.
-          </blockquote>
-
-          <cite className="block mt-4 text-xs font-bold text-right uppercase">
-            – Thomas Nagel
-          </cite>
+      <div className="py-4">
+    <div className="shadow-lg group container  rounded-md bg-white  max-w-sm flex justify-center items-center  mx-auto content-div">
+        <div>
+        <div  className="w-full image-cover rounded-t-md" >
+          <div className="p-2 m-4 w-16 h-16 text-center bg-gray-700 rounded-full text-white float-right fd-cl group-hover:opacity-25">
+             <span className="text-base tracking-wide  font-bold border-b border-white font-sans"> 12</span>
+              <span className="text-xs tracking-wide font-bold uppercase block font-sans">April</span>
+          </div>
         </div>
-
-        <figure className="w-2/3 md:w-1/3">
-          <img alt="A dog relaxing" src={dogIllustration} />
-        </figure>
+        <div className="py-8 px-4 bg-white  rounded-b-md fd-cl group-hover:opacity-25">
+            <span className="block text-lg text-gray-800 font-bold tracking-wide">Classic product photos</span>
+            <span  className="block text-gray-600 text-sm">Clear, crisp, pure white backgrounds. Professionally shot and edited in our studio. View the classic product photography pricing table. </span>
+            <span  className="block text-gray-600 text-sm">As low as $30 per photo </span>
+        </div>
+        </div>
+        <div className="absolute opacity-0 fd-sh group-hover:opacity-100">
+       <span className="text-3xl font-bold text-white tracking-wider leading-relaxed font-sans">Classic product photos</span> 
+       <div className="pt-8 text-center">
+           <button className="text-center rounded-lg p-4 bg-white font-sans text-gray-700 font-bold text-lg">Learn more</button>
+           </div>
+    </div>
+    </div>
+   
+</div>
+<style>{"\
+  .content-div{\
+  background-image:url('https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80');\
+  background-repeat: no-repeat;\
+  background-size: cover;\
+  background-position:center;\
+}\
+  .content-div:hover{\
+    background-image:\
+    linear-gradient(to right,\
+     rgba(126, 213, 111, 0.801), hsla(160, 64%, 43%, 0.801) ),\
+     url('https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80');\
+    }\
+    .image-cover  {\
+      height:260px;\
+    }\
+    .content-div:hover .fd-cl{ \
+ opacity: 0.25;\
+    }\
+    .content-div:hover .fd-sh{ \
+ opacity: 1;\
+    }\
+"}</style>
       </section>
     </Layout>
   );
